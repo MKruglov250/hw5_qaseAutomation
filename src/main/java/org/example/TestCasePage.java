4a099ca4942bd9975c6255962fff35ac692831af
mkruglov <mrmarkussss@gmail.com>
2023-11-20 22:34:11 +0200

    Created test cases for Test Cases module

--- src/main/java/org/example/TestCasePage.java
+++ src/main/java/org/example/TestCasePage.java
@@ -38,12 +38,22 @@ public class TestCasePage {
     SelenideElement behaviourElement = pageDropdowns.get(8);
     SelenideElement automationStatusElement = pageDropdowns.get(9);
     SelenideElement titleInput = $x("//input[@name='title']");
-    SelenideElement descriptionInput = $x("//p[contains(@data-placeholder,'For example')]");
-    SelenideElement preconditionInput = $$("p[data-placeholder='Empty value']").get(0);
-    SelenideElement postconditionInput = $$("p[data-placeholder='Empty value']").get(0);
-    SelenideElement stepActionInput = $x("//p[@data-placeholder='Step Action']");
-    SelenideElement stepDataInput = $x("//p[@data-placeholder='Data']");
-    SelenideElement stepResultInput = $x("//p[@data-placeholder='Expected result']");
+    SelenideElement descriptionInput = $x("//label[@for='0-description']" +
+                    "/parent::div//div[@class='ProseMirror toastui-editor-contents']/p");
+    SelenideElement preconditionInput = $x("//label[@for='0-preconditions']" +
+            "/parent::div//div[@class='ProseMirror toastui-editor-contents']/p");
+    SelenideElement postconditionInput = $x("//label[@for='0-postconditions']" +
+            "/parent::div//div[@class='ProseMirror toastui-editor-contents']/p");
+    SelenideElement stepActionInput = $x("(//div[@class='case-create-block steps-block']" +
+            "/following-sibling::div//p)[1]");
+    SelenideElement stepDataInput = $x("(//div[@class='case-create-block steps-block']" +
+            "/following-sibling::div//p)[2]");
+    SelenideElement stepResultInput = $x("(//div[@class='case-create-block steps-block']" +
+            "/following-sibling::div//p)[3]");
+    SelenideElement editControl = $x("//a[contains(@href,'/case/QASEAPP/edit')]");
+    SelenideElement deleteControl = editControl.sibling(1);
+    SelenideElement deleteButton = $x("//*[text()='Delete']");
+    SelenideElement deleteSuccessMessage = $x("//*[text()[contains(.,' was successfully deleted')]]");
 
     public TestCasePage() throws ParserConfigurationException, IOException, SAXException {
     }
@@ -61,7 +71,7 @@ public class TestCasePage {
         setStatus();
         setSuite();
         setSeverity();
-        setPrority();
+        setPriority();
         setType();
         setLayer();
         setIsFlaky();
@@ -76,101 +86,174 @@ public class TestCasePage {
         setStepResult();
     }
 
+    @Step("Set Test Case Title")
     public void setTitle(){
         titleInput.setValue(firstCaseModel.getTitle());
+        log.debug("Title set to: " + firstCaseModel.getTitle());
     }
 
+    @Step("Set Test Case Status")
     public void setStatus(){
         statusElement.sendKeys(firstCaseModel.getStatus());
         statusElement.sendKeys(Keys.ARROW_DOWN);
         statusElement.sendKeys(Keys.ENTER);
+        log.debug("Status set to: " + firstCaseModel.getStatus());
     }
 
+    @Step("Set Test Case Description")
     public void setDescription(){
         descriptionInput.setValue(firstCaseModel.getDescription());
+        log.debug("Description set to: " + firstCaseModel.getDescription());
     }
 
+    @Step("Set Test Case Description")
+    public void setDescription(String description){
+        descriptionInput.setValue(description);
+        log.debug("Description set to: " + description);
+    }
+
+    @Step("Set Test Case Suite")
     public void setSuite(){
         suiteElement.sendKeys(firstCaseModel.getSuite());
         suiteElement.sendKeys(Keys.ARROW_DOWN);
         suiteElement.sendKeys(Keys.ENTER);
+        log.debug("Suite set to: " + firstCaseModel.getSuite());
     }
 
+    @Step("Set Test Case Severity")
     public void setSeverity(){
         severityElement.sendKeys(firstCaseModel.getSeverity());
         severityElement.sendKeys(Keys.ARROW_DOWN);
         severityElement.sendKeys(Keys.ENTER);
+        log.debug("Severity set to: " + firstCaseModel.getSeverity());
     }
 
-    public void setPrority(){
+    @Step("Set Test Case Priority")
+    public void setPriority(){
         priorityElement.sendKeys(firstCaseModel.getPriority());
         priorityElement.sendKeys(Keys.ARROW_DOWN);
         priorityElement.sendKeys(Keys.ENTER);
+        log.debug("Priority set to: " + firstCaseModel.getPriority());
     }
 
+    @Step("Set Test Case Type")
     public void setType(){
         typeElement.sendKeys(firstCaseModel.getType());
         typeElement.sendKeys(Keys.ARROW_DOWN);
         typeElement.sendKeys(Keys.ENTER);
+        log.debug("Type set to: " + firstCaseModel.getType());
     }
 
+    @Step("Set Test Case Layer")
     public void setLayer(){
         layerElement.sendKeys(firstCaseModel.getLayer());
         layerElement.sendKeys(Keys.ARROW_DOWN);
         layerElement.sendKeys(Keys.ENTER);
+        log.debug("Layer set to: " + firstCaseModel.getLayer());
     }
 
+    @Step("Set Test Case IsFlaky")
     public void setIsFlaky(){
         isFlakyElement.sendKeys(firstCaseModel.getIsFlaky());
         isFlakyElement.sendKeys(Keys.ARROW_DOWN);
         isFlakyElement.sendKeys(Keys.ENTER);
+        log.debug("IsFlaky field set to: " + firstCaseModel.getIsFlaky());
     }
 
+    @Step("Set Test Case Milestone")
     public void setMilestone(){
         milestoneElement.sendKeys(firstCaseModel.getMilestone());
         milestoneElement.sendKeys(Keys.ARROW_DOWN);
         milestoneElement.sendKeys(Keys.ENTER);
+        log.debug("Milestone set to: " + firstCaseModel.getMilestone());
     }
 
+    @Step("Set Test Case Behaviour")
     public void setBehaviour(){
         behaviourElement.sendKeys(firstCaseModel.getBehaviour());
         behaviourElement.sendKeys(Keys.ARROW_DOWN);
         behaviourElement.sendKeys(Keys.ENTER);
+        log.debug("Behaviour set to: " + firstCaseModel.getBehaviour());
     }
 
+    @Step("Set Test Case Automation Status")
     public void setAutomationStatus(){
         automationStatusElement.sendKeys(firstCaseModel.getAutomationStatus());
         automationStatusElement.sendKeys(Keys.ARROW_DOWN);
         automationStatusElement.sendKeys(Keys.ENTER);
+        log.debug("Automation Status set to: " + firstCaseModel.getAutomationStatus());
     }
 
+    @Step("Set Test Case Preconditions")
     public void setPrecondition(){
         preconditionInput.setValue(firstCaseModel.getPreconditions());
+        log.debug("Pre-conditions set to: " + firstCaseModel.getPreconditions());
     }
 
+    @Step("Set Test Case Postconditions")
     public void setPostcondition(){
         postconditionInput.setValue(firstCaseModel.getPostconditions());
+        log.debug("Post-conditions set to: " + firstCaseModel.getPostconditions());
     }
 
+    @Step("Add new Test Case Step")
     public void clickAddStep(){
         addStep.click();
+        log.debug("Added new step");
     }
 
+    @Step("Set Step action")
     public void setStepAction(){
         stepActionInput.setValue(firstCaseModel.getTestStepOneName());
+        log.debug("Step action set to: " + firstCaseModel.getTestStepOneName());
     }
 
+    @Step("Set Step Data")
     public void setStepData(){
         stepDataInput.setValue(firstCaseModel.getTestStepOneData());
+        log.debug("Step data set to: " + firstCaseModel.getTestStepOneData());
     }
 
+    @Step("Set Step Result")
     public void setStepResult(){
         stepResultInput.setValue(firstCaseModel.getTestStepOneResult());
+        log.debug("Step result to: " + firstCaseModel.getTestStepOneResult());
     }
 
     @Step("Saving test case with created data")
     public void saveTestCase(){
         saveButton.click();
+        log.debug("Saving Test Case Data - God bless us all!");
+    }
+
+    @Step("Opening Test Case")
+    public void openTestCase(){
+        firstTestCaseElement.click();
+        log.debug("Clicked on Test Case 1");
+    }
+
+    @Step("Clicking Edit Control")
+    public void editTestCase(){
+        editControl.click();
+        log.debug("Opened test case for Edit");
+    }
+
+    @Step("Clicking Delete Control")
+    public void deleteTestCase(){
+        deleteControl.click();
+        log.debug("Deleting test case");
+    }
+
+    @Step("Confirm Deleting Test Case")
+    public void confirmDeletion(){
+        deleteButton.click();
+        log.debug("Confirmed test case deletion");
+    }
+
+    @Step("Check delete message")
+    public boolean checkDeleteMessage(){
+        log.debug("Getting Delete Success Message");
+        return deleteSuccessMessage.exists();
     }
 
 }
--- src/test/java/org/example/TestCasePageTest.java
+++ src/test/java/org/example/TestCasePageTest.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import lombok.extern.log4j.Log4j2;
+import org.example.model.UserModel;
+import org.example.utilities.LoginUtils;
 import org.json.simple.parser.ParseException;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
@@ -11,6 +16,7 @@ import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.open;
 
+@Log4j2
 public class TestCasePageTest extends BaseTest {
 
     Login login = new Login();
@@ -19,19 +25,61 @@ public class TestCasePageTest extends BaseTest {
     public TestCasePageTest() throws ParserConfigurationException, IOException, SAXException {
     }
 
-    @BeforeClass(description = "Login before performing Test Case module tests",
+    @BeforeMethod(description = "Login before performing Test Case module tests",
     alwaysRun = true)
-    public void beforeClass() throws IOException, ParseException {
+    public void beforeMethod() throws IOException, ParseException {
         login.loginToSiteValid();
+        log.info("Logged in to Qase.io");
         open("project/QASEAPP");
     }
 
     @Test(description = "Check Creation of Test Case", groups = "Smoke")
     public void testCreateTestCase(){
         testCasePage.newTestCase();
+        log.info("Create New Test Case window opened");
         testCasePage.enterTestCaseData();
+        log.info("Test Case Info Entered");
         testCasePage.saveTestCase();
+        log.info("Save button pressed");
         Assert.assertTrue(testCasePage.firstTestCaseElement.exists());
+    }
+
+    @Test(description = "Check Reading Test Case", groups = "Smoke",
+            priority = 1)
+    public void testReadTestCaseOne(){
+        testCasePage.openTestCase();
+        testCasePage.editTestCase();
+        Assert.assertEquals(testCasePage.titleInput.getValue(),
+                "First Test Case");
+    }
+
+    @Test(description = "Check Editing Test Case", groups = "Regression",
+            priority = 2)
+    public void testEditTestCaseOne(){
+        testCasePage.openTestCase();
+        testCasePage.editTestCase();
+        testCasePage.setDescription("Brand-new edited description");
+        testCasePage.saveTestCase();
+        testCasePage.editTestCase();
+        Assert.assertEquals(testCasePage.descriptionInput.getText(),
+                "Brand-new edited description");
+    }
+
+    @Test(description = "Check Deleting test case", groups = "Regression",
+            priority = 3)
+    public void testDeleteTestCaseOne(){
+        log.info("Checking Test Case Deletion");
+        testCasePage.openTestCase();
+        testCasePage.deleteTestCase();
+        testCasePage.confirmDeletion();
+        boolean deleteMessageExists = testCasePage.checkDeleteMessage();
+        Assert.assertTrue(deleteMessageExists);
+        log.info("Delete message exists: ");
+    }
+
+    @AfterMethod(description = "Logging out after performing test")
+    public void logout(){
+        LoginUtils.logout();
     }
 
 }
